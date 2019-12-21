dd3533308599f74f82f1f4282813f6ad3798ab5e
ferocity <nightofferocity@gmail.com>
2019-12-21 15:45:09 +0500

    штвучюоы

--- Scripts/FormTeacher.js
+++ Scripts/FormTeacher.js
@@ -1,3 +1,12 @@
-function generate() {
-    document.getElementById()
+function main(){
+    let name = document.getElementsById('name');
+    let subject = document.getElementsById('subject');
+    let theme = document.getElementsById('theme');
+    let groupNumber = document.getElementsById('class number');
+    generate(name, groupNumber, theme)
+}
+
+function generate(name = '1', groupNumber = Math.floor(Math.random() * 10), theme = 'WT') {
+    console.log(name[0] + groupNumber + theme[0] + Math.floor(Math.random() * 100))
+    return name[0] + groupNumber + theme[0] + Math.floor(Math.random() * 100)
 }
